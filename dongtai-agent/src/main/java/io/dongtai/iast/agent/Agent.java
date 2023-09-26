c02a3a2e00d29aacd06be6a07527749a8516e851
cc11001100 <cc11001100@qq.com>
2023-09-26 16:19:11 +0800

    refactor: Agent

--- dongtai-agent/src/main/java/io/dongtai/iast/agent/Agent.java
+++ dongtai-agent/src/main/java/io/dongtai/iast/agent/Agent.java
@@ -11,17 +11,35 @@ import java.util.Arrays;
 import java.util.Map;
 
 /**
+ * 命令行入口
+ *
  * @author dongzhiyong@huoxian.cn
  */
 public class Agent {
 
+    // agent jar包的路径
     private static final String AGENT_PATH = Agent.class.getProtectionDomain().getCodeSource().getLocation().getFile();
+
+    // 操作系统的名字
     private static final String OS_NAME = System.getProperty("os.name").toLowerCase();
+
+    // jattach文件的路径，注入是依赖的一个第三方的库
+    // https://github.com/jattach/jattach
     private static String JATTACH_FILE;
 
+    /**
+     * 解析命令行传递的参数
+     *
+     * @param args
+     * @return
+     * @throws ParseException
+     */
     private static String[] parseAgentArgs(String[] args) throws ParseException {
         Options attachOptions = new Options();
 
+        // TODO 部分参数可能需要再详细一些,·比如：
+        // 1. 每条命令应该有详细的作用说明
+        // 2. 每套命令应该有具体的值示例
         attachOptions.addOption(build("p", "pid", "webserver process id"));
         attachOptions.addOption(build("m", "mode", "optional: install uninstall"));
         attachOptions.addOption(build("debug", "debug", "optional: debug mode"));
@@ -65,11 +83,18 @@ public class Agent {
             }
             return new String[]{pid, attachArgs.toString()};
         } else {
+            // TODO 2023-9-26 16:10:10 参数传递错误的时候打印消息
             formatter.printHelp("java -jar agent.jar", attachOptions, true);
             return null;
         }
     }
 
+    /**
+     * 附加用户进程
+     *
+     * @param pid
+     * @param agentArgs
+     */
     private static void doAttach(String pid, String agentArgs) {
         String[] execution = {
                 JATTACH_FILE,
@@ -106,34 +131,40 @@ public class Agent {
         return OS_NAME.contains("mac") && OS_NAME.indexOf("os") > 0;
     }
 
+    /**
+     * 把jattach解包到workspace以便等下运行它
+     *
+     * @throws IOException
+     */
     private static void extractJattach() throws IOException {
-        String tmpDir = IastProperties.initTmpDir();
+        String workspace = IastProperties.getInstance().getWorkspace();
         if (isWindows()) {
-            JATTACH_FILE = tmpDir + "jattach.exe";
+            JATTACH_FILE = workspace + "jattach.exe";
             FileUtils.getResourceToFile("bin/jattach.exe", JATTACH_FILE);
         } else if (isMacOs()) {
-            JATTACH_FILE = tmpDir + "jattach-mac";
+            JATTACH_FILE = workspace + "jattach-mac";
             FileUtils.getResourceToFile("bin/jattach-mac", JATTACH_FILE);
         } else if (isArm()) {
-            JATTACH_FILE = tmpDir + "jattach-arm";
+            JATTACH_FILE = workspace + "jattach-arm";
             FileUtils.getResourceToFile("bin/jattach-arm", JATTACH_FILE);
         } else {
-            JATTACH_FILE = tmpDir + "jattach-linux";
+            JATTACH_FILE = workspace + "jattach-linux";
             FileUtils.getResourceToFile("bin/jattach-linux", JATTACH_FILE);
         }
         if ((new File(JATTACH_FILE)).setExecutable(true)) {
-            DongTaiLog.info("jattach extract success. wait for attach");
+            DongTaiLog.info("jattach extract to {} success. wait for attach", JATTACH_FILE);
         } else {
             DongTaiLog.error(ErrorCode.JATTACH_EXTRACT_FAILED, JATTACH_FILE);
         }
     }
 
     /**
-     * 解析参数、加入tools.jar、调用attach
+     * 命令行运行的入口
      *
      * @param args
      */
     public static void main(String[] args) throws InterruptedException {
+        // TODO 更友好的处理用户输入的参数，提示更完善一点之类的
         String[] agentArgs = new String[0];
         try {
             agentArgs = parseAgentArgs(args);
