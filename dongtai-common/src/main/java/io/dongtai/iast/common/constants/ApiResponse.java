b187df5e7a1d75de256b7a79dc91491d4113dea0
cc11001100 <cc11001100@qq.com>
2023-09-26 18:34:16 +0800

    feat: add ApiResponse

---
+++ dongtai-common/src/main/java/io/dongtai/iast/common/constants/ApiResponse.java
@@ -0,0 +1,12 @@
+package io.dongtai.iast.common.constants;
+
+/**
+ * @author CC11001100
+ * @since v1.15.0
+ */
+public class ApiResponse {
+
+    // api正常响应的响应码，别他妈问我为啥是这个，从众测平台就祖传下来的...
+    public static final int CODE_OK = 201;
+
+}
