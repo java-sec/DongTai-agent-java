3d33bd5aa50ca5ccf36cd36d479a40a76b86588f
cc11001100 <cc11001100@qq.com>
2023-09-27 16:12:39 +0800

    add todo for ShutdownThread

--- dongtai-agent/src/main/java/io/dongtai/iast/agent/ShutdownThread.java
+++ dongtai-agent/src/main/java/io/dongtai/iast/agent/ShutdownThread.java
@@ -3,11 +3,14 @@ package io.dongtai.iast.agent;
 import io.dongtai.iast.agent.manager.EngineManager;
 import io.dongtai.iast.agent.monitor.MonitorDaemonThread;
 
+/**
+ * TODO 在卸载的时候是否有必要执行这么一个hook呢？JVM都要退出了还卸载干嘛？
+ */
 public class ShutdownThread extends Thread {
 
     @Override
     public void run() {
-        if (!MonitorDaemonThread.isExit){
+        if (!MonitorDaemonThread.isExit) {
             EngineManager.getInstance().uninstall();
         }
     }
