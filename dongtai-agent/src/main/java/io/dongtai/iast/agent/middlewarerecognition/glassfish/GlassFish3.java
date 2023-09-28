bfc09443e4d89dd5bc739e93b636f1a8b4515b3d
cc11001100 <cc11001100@qq.com>
2023-09-28 14:30:39 +0800

    comment and add todo for GlassFish

--- dongtai-agent/src/main/java/io/dongtai/iast/agent/middlewarerecognition/glassfish/GlassFish3.java
+++ dongtai-agent/src/main/java/io/dongtai/iast/agent/middlewarerecognition/glassfish/GlassFish3.java
@@ -7,28 +7,34 @@ import java.lang.management.RuntimeMXBean;
 import java.net.URL;
 
 /**
+ * TODO 2023-9-28 14:26:52 不知道什么原因没有开启对这个Web容器的支持，调研一下同时确认是否需要继续增加对此容器的支持
+ *
  * @author dongzhiyong@huoxian.cn
+ * @see GlassFish4
  */
+@Deprecated
 public class GlassFish3 implements IServer {
-    public static final String HTTP_SERVER = " org/glassfish/grizzly/http/server/HttpServer.class".substring(1);
 
     @Override
     public boolean isMatch(RuntimeMXBean runtimeMXBean, ClassLoader loader) {
         try {
-            URL url = loader.getResource(HTTP_SERVER);
-            return (url != null);
-        } catch (Throwable ignoreable) {
+            URL url = loader.getResource(" org/glassfish/grizzly/http/server/HttpServer.class".substring(1));
+            return url != null;
+        } catch (Throwable ignore) {
             return false;
         }
     }
 
     @Override
     public String getName() {
+        // TODO 2023-9-28 14:22:27 这个名字其实应该是 "Grizzly" ？
         return "grizzly";
     }
 
     @Override
     public String getVersion() {
+        // TODO 2023-9-28 14:22:19 此处的版本号对吗？为啥是返回了一个名字，需要与Server端开发确认是否是有什么历史遗留原因
         return "Grizzly";
     }
+
 }
--- dongtai-agent/src/main/java/io/dongtai/iast/agent/middlewarerecognition/glassfish/GlassFish4.java
+++ dongtai-agent/src/main/java/io/dongtai/iast/agent/middlewarerecognition/glassfish/GlassFish4.java
@@ -2,6 +2,8 @@ package io.dongtai.iast.agent.middlewarerecognition.glassfish;
 
 /**
  * @author dongzhiyong@huoxian.cn
+ * @see GlassFish3
  */
+@Deprecated
 public class GlassFish4 {
 }
