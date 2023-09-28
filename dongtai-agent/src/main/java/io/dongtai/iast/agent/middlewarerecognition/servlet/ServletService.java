a3304b8e13bf0480f806884ab2b2b0663738d885
cc11001100 <cc11001100@qq.com>
2023-09-28 15:12:41 +0800

    comment for ServletService

--- dongtai-agent/src/main/java/io/dongtai/iast/agent/middlewarerecognition/servlet/ServletService.java
+++ dongtai-agent/src/main/java/io/dongtai/iast/agent/middlewarerecognition/servlet/ServletService.java
@@ -4,29 +4,40 @@ import io.dongtai.iast.agent.middlewarerecognition.IServer;
 
 import java.lang.management.RuntimeMXBean;
 
+/**
+ * @author dongzhiyong@huoxian.cn
+ */
 public class ServletService implements IServer {
+
+    public static final String NAME = "Servlet";
+
     @Override
     public boolean isMatch(RuntimeMXBean paramRuntimeMXBean, ClassLoader loader) {
+
         try {
-            loader.loadClass("javax.servlet.ServletRequest");
+            loader.loadClass(" javax.servlet.ServletRequest".substring(1));
             return true;
         } catch (Throwable ignore) {
         }
+
         try {
-            loader.loadClass("jakarta.servlet.ServletRequest");
+            // tomcat 10之后的Servlet改为了这个
+            loader.loadClass(" jakarta.servlet.ServletRequest".substring(1));
             return true;
         } catch (Throwable ignore) {
         }
+
         return false;
     }
 
     @Override
     public String getName() {
-        return "Servlet";
+        return NAME;
     }
 
     @Override
     public String getVersion() {
+        // TODO 2023-9-28 14:31:56 为什么其它的实现没有版本的时候返回的是null这里返回的却是空字符串？是不是有什么特殊原因
         return "";
     }
 }
