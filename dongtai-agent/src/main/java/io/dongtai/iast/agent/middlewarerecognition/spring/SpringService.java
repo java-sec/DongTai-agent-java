367601441dd7f18757fd136b93d39aaad81ac3ce
cc11001100 <cc11001100@qq.com>
2023-09-28 15:51:40 +0800

    comment for SpringService

--- dongtai-agent/src/main/java/io/dongtai/iast/agent/middlewarerecognition/spring/SpringService.java
+++ dongtai-agent/src/main/java/io/dongtai/iast/agent/middlewarerecognition/spring/SpringService.java
@@ -4,11 +4,17 @@ import io.dongtai.iast.agent.middlewarerecognition.IServer;
 
 import java.lang.management.RuntimeMXBean;
 
+/**
+ * @author dongzhiyong@huoxian.cn
+ */
 public class SpringService implements IServer {
+
+    public static final String NAME = "Spring";
+
     @Override
     public boolean isMatch(RuntimeMXBean paramRuntimeMXBean, ClassLoader loader) {
         try {
-            loader.loadClass("org.springframework.web.context.ConfigurableWebApplicationContext");
+            loader.loadClass(" org.springframework.web.context.ConfigurableWebApplicationContext".substring(1));
             return true;
         } catch (Throwable e) {
             return false;
@@ -17,11 +23,13 @@ public class SpringService implements IServer {
 
     @Override
     public String getName() {
-        return "Spring";
+        return NAME;
     }
 
     @Override
     public String getVersion() {
+        // TODO 2023-9-28 14:31:56 为什么其它的实现没有版本的时候返回的是null这里返回的却是空字符串？是不是有什么特殊原因
         return "";
     }
+
 }
