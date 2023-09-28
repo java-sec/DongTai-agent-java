eaf1e26749fef76d2320f46557d092d14d806dbd
cc11001100 <cc11001100@qq.com>
2023-09-28 14:18:49 +0800

    comment DubboService

--- dongtai-agent/src/main/java/io/dongtai/iast/agent/middlewarerecognition/dubbo/DubboService.java
+++ dongtai-agent/src/main/java/io/dongtai/iast/agent/middlewarerecognition/dubbo/DubboService.java
@@ -4,19 +4,29 @@ import io.dongtai.iast.agent.middlewarerecognition.IServer;
 
 import java.lang.management.RuntimeMXBean;
 
+/**
+ * 检查是否是dubbo服务
+ */
 public class DubboService implements IServer {
+
     @Override
     public boolean isMatch(RuntimeMXBean paramRuntimeMXBean, ClassLoader loader) {
+
         try {
+            // 后面这个apache的版本会使用得越来越多，所以此项放在前面优先命中
+            // 2.7.0开始，直接使用org.apache.dubbo
             loader.loadClass(" org.apache.dubbo.monitor.support.MonitorFilter".substring(1));
             return true;
         } catch (Throwable ignored) {
         }
+
         try {
+            // 如果2.6.x及以下版本，可以使用：com.alibaba.dubbo，
             loader.loadClass(" com.alibaba.dubbo.monitor.support.MonitorFilter".substring(1));
             return true;
         } catch (Throwable ignored) {
         }
+
         return false;
     }
 
@@ -29,4 +39,5 @@ public class DubboService implements IServer {
     public String getVersion() {
         return null;
     }
+
 }
