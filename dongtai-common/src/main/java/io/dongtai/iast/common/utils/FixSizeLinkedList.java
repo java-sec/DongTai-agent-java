e910eaf9ef04e4f2174df70b58c5e58f92725247
cc11001100 <cc11001100@qq.com>
2023-10-08 15:00:57 +0800

    Deprecated FixSizeLinkedList

--- dongtai-common/src/main/java/io/dongtai/iast/common/utils/FixSizeLinkedList.java
+++ dongtai-common/src/main/java/io/dongtai/iast/common/utils/FixSizeLinkedList.java
@@ -5,10 +5,15 @@ import java.util.LinkedList;
 /**
  * 固定大小链表
  *
+ *
+ * TODO 这个工具类好像没有用到，后面考虑移除掉
+ *
  * @author liyuan40
  * @date 2022/3/9 20:16
  */
+@Deprecated
 public class FixSizeLinkedList<T> extends LinkedList<T> {
+
     private static final long serialVersionUID = 6147000002339841725L;
     private final int capacity;
 
@@ -24,4 +29,5 @@ public class FixSizeLinkedList<T> extends LinkedList<T> {
         }
         return super.add(t);
     }
+
 }
