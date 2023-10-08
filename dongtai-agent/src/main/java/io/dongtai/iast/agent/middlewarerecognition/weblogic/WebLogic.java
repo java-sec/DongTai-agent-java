b95e7404e111b15f58fbf82a952b5f5c27ab6e75
cc11001100 <cc11001100@qq.com>
2023-10-08 12:12:43 +0800

    WebLogic review & add todo

--- dongtai-agent/src/main/java/io/dongtai/iast/agent/middlewarerecognition/weblogic/WebLogic.java
+++ dongtai-agent/src/main/java/io/dongtai/iast/agent/middlewarerecognition/weblogic/WebLogic.java
@@ -7,11 +7,17 @@ import java.io.File;
 import java.lang.management.RuntimeMXBean;
 
 /**
+ * 识别web logic服务
+ *
  * @author dongzhiyong@huoxian.cn
+ * @author CC11001100
  */
 public class WebLogic implements IServer {
+
+    public static final String NAME = "WebLogic";
+
     @Override
-    public boolean isMatch(RuntimeMXBean paramRuntimeMXBean, ClassLoader loader) {
+    public boolean isMatch(RuntimeMXBean runtimeMXBean, ClassLoader loader) {
         File runFile = new File(".", "bin/startWebLogic.sh");
         File configFile = new File(".", "init-info/domain-info.xml");
         System.setProperty("UseSunHttpHandler", "true");
@@ -20,12 +26,14 @@ public class WebLogic implements IServer {
 
     @Override
     public String getName() {
-        return "WebLogic";
+        return NAME;
     }
 
     @Override
     public String getVersion() {
-        // 从xml中解析版本
-        return "WebLogic";
+        // TODO 从xml中解析版本，这里直接返回显然是不对的，先直接置为null
+//        return "WebLogic";
+        return null;
     }
+
 }
