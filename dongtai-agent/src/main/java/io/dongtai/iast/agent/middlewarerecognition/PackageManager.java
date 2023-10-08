051e0bd89d66ba8be51e1bd85b069b47e0d64477
cc11001100 <cc11001100@qq.com>
2023-10-08 11:40:55 +0800

    comment for PackageManager

--- dongtai-agent/src/main/java/io/dongtai/iast/agent/middlewarerecognition/PackageManager.java
+++ dongtai-agent/src/main/java/io/dongtai/iast/agent/middlewarerecognition/PackageManager.java
@@ -2,9 +2,15 @@ package io.dongtai.iast.agent.middlewarerecognition;
 
 
 /**
+ * 用于检测当前的ClassLoader是否能加载给定的类，后面会有一些服务识别通过是否存在这个Class来判断是否有对应的服务
+ * <p>
+ * TODO 调研一下这样子搞的准确度，会不会出现仅仅只是依赖里有相关Class但是实际上并没有被调用，然后就误判了...
+ *
  * @author dongzhiyong@huoxian.cn
  */
 public final class PackageManager {
+
+    // 要识别的类名
     private final String classname;
 
     public PackageManager(String classname) {
@@ -19,5 +25,6 @@ public final class PackageManager {
             return null;
         }
     }
+
 }
 
