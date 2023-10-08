a8c4a6c74d6523ade22e02faa710bb25cc3acdef
cc11001100 <cc11001100@qq.com>
2023-10-08 12:13:16 +0800

    DubboService override param rename

--- dongtai-agent/src/main/java/io/dongtai/iast/agent/middlewarerecognition/dubbo/DubboService.java
+++ dongtai-agent/src/main/java/io/dongtai/iast/agent/middlewarerecognition/dubbo/DubboService.java
@@ -6,13 +6,15 @@ import java.lang.management.RuntimeMXBean;
 
 /**
  * 检查是否是dubbo服务
+ *
+ * @author CC11001100
  */
 public class DubboService implements IServer {
 
     public static final String NAME = "Dubbo";
 
     @Override
-    public boolean isMatch(RuntimeMXBean paramRuntimeMXBean, ClassLoader loader) {
+    public boolean isMatch(RuntimeMXBean runtimeMXBean, ClassLoader loader) {
 
         try {
             // 后面这个apache的版本会使用得越来越多，所以此项放在前面优先命中
