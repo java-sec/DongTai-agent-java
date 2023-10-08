c531eef4fb3d09ef97e35357d96308b31c4345d3
cc11001100 <cc11001100@qq.com>
2023-10-08 11:09:56 +0800

    ServerDetect 删除无用方法 setServers

--- dongtai-agent/src/main/java/io/dongtai/iast/agent/middlewarerecognition/ServerDetect.java
+++ dongtai-agent/src/main/java/io/dongtai/iast/agent/middlewarerecognition/ServerDetect.java
@@ -12,16 +12,22 @@ import io.dongtai.iast.agent.middlewarerecognition.spring.Tomcat;
 import io.dongtai.iast.agent.middlewarerecognition.tomcat.*;
 import io.dongtai.iast.agent.middlewarerecognition.weblogic.WebLogic;
 import io.dongtai.iast.agent.middlewarerecognition.websphere.WebSphere;
+import io.dongtai.log.DongTaiLog;
 
 import java.io.File;
 import java.lang.management.ManagementFactory;
 import java.lang.management.RuntimeMXBean;
 
 /**
+ * 用于检测当前运行的服务
+ *
  * @author dongzhiyong@huoxian.cn
  */
 public class ServerDetect {
-    private static IServer[] SERVERS = {
+
+    // 注意，此处的注册顺序最好是要跟常见程序对应的，这样匹配的时候效率会高一些
+    // 这里的顺序不要随意调整，因为匹配原理可能会导致调整之后反而识别错误，现在这样也能凑活着工作反正...
+    private static IServer[] servers = {
             new Tomcat(),
             new TomcatV9(),
             new TomcatV8(),
@@ -40,21 +46,28 @@ public class ServerDetect {
             new UnknownService()
     };
 
-    public static void setSERVERS(IServer[] SERVERS) {
-        ServerDetect.SERVERS = SERVERS;
-    }
-
+    /**
+     * 获取当前使用的WebServer服务是啥
+     *
+     * @return
+     */
     public static IServer getWebserver() {
-        if (AgentLauncher.LAUNCH_MODE.equals(AgentLauncher.LAUNCH_MODE_ATTACH)) {
+
+        // TODO 没看懂这个检查是什么原因
+        if (AgentLauncher.launchMode == AgentLauncher.LaunchMode.ATTACH) {
+            DongTaiLog.info("io.dongtai.iast.agent.middlewarerecognition.ServerDetect.getWebserver return UnknownService, because it's in attach mode");
             return new UnknownService();
         }
+
+        // 然后开始逐个匹配
         RuntimeMXBean runtimeMXBean = ManagementFactory.getRuntimeMXBean();
         ClassLoader loader = Thread.currentThread().getContextClassLoader();
-        for (IServer server : SERVERS) {
+        for (IServer server : servers) {
             if (server.isMatch(runtimeMXBean, loader)) {
                 return server;
             }
         }
+
         return null;
     }
 
